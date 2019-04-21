7f47a3520d9f1f6c4f3a0c29922388a5c9b30c86
佐藤　帆矢 <satouhonoya@satouhoyanoMacBook-Pro.local>
2019-04-21 22:39:53 +0900

    finish kadai

---
+++ facebook.rb
@@ -0,0 +1,14 @@
+
+1..101.times do |i|    
+
+    if i%15 ==0
+        puts "facebook"
+    elsif i%3 ==0
+        puts "face!"
+    elsif i%5 ==0 
+        puts "book!"
+    else
+        puts i
+    end
+
+end
